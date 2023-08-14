051ca500dbb742444560e2fcc2e7edc3da5a5de3
Dener <mundoem3di@gmail.com>
2023-08-14 18:58:43 -0300

    update api

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
@@ -9,25 +8,25 @@ app.use(express.json()); // Middleware para fazer o parse de requisições com c
 
 // Rota para receber os dados do formulário
 app.post('/enviar-email', (req, res) => {
+  
   const dadosDoFormulario = req.body;
-
 
   const configSmtp = nodemailer.createTransport({
     host: 'smtp.office365.com',
     port: '587',
     secure: false, // true for 465 else false for others
     auth: {
-        user: "seu email source",
-        pass: "senha de login"
+        user: "dener_bat@outlook.com",
+        pass: "Mara123#"
     }
 })
 
- // Configurando o envio do e-mail com os dados do formulário
+ // Configurando o envio do e-mail com os dados do formulário recebidos do front end
  const emailContent = `
- <h1>Dados do formulário recebidos:</h1>
- <p>Nome: ${dadosDoFormulario.name}</p>
- <p>Email: ${dadosDoFormulario.mail}</p>
- <p>Mensagem: ${dadosDoFormulario.message}</p>
+ <h1>Contato via site:</h1>
+ <h2>Nome: ${dadosDoFormulario.name}</h2>
+ <h2>Email: ${dadosDoFormulario.mail}</h2>
+ <h3>Mensagem: ${dadosDoFormulario.message}</h3>
 `;
 
 console.log("dados recebidos", dadosDoFormulario)
@@ -36,24 +35,24 @@ console.log("dados do emailContent", emailContent)
 // configurando o envio do email
 
 configSmtp.sendMail({
-    from: "de quem eh o email <dener_bat@outlook.com>",
-    to: "denerag91@gmail.com",
-    subject: "Assunto do email",
+    from: "Form site portofolio <dener_bat@outlook.com>",
+    to: "dener.criarbr@gmail.com",
+    subject: "Contato via site",
     html: emailContent,
-    text: 'Caso o email nao renderize o html sera mostrado esse texto'
+    text: `"Nome:" ${dadosDoFormulario.name}, "E-mail:" ${dadosDoFormulario.mail}, "Mensagem:" ${dadosDoFormulario.message}`
 })
 // mostrando erros e seu deu certo
 .then((resp) => {
-  console.log(resp, "email foi enviado")
-  res.json({ message: 'Dados do formulário recebidos com sucesso no servidor, essa eh uma resposta dele!' });
+  console.log(resp, "Mensagem enviada com sucesso")
+  res.json({ message: 'Mensagem enviada'});
 })
 
 .catch((err)=> {
 console.log(err, "seu email deu erro")
-res.status(500).json({ error: 'Erro ao enviar o email.' });
+res.status(500).json({ error: "Falha no envio, tente novamente 2" });
 })
 });
 
 app.listen(3000, () => {
-  console.log('Servidor backend online: http://192.177.234.11:3000');
+  console.log('Servidor backend online!');
 });
